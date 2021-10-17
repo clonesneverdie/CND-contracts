5801a7d4a466edca4971088a1684e0d8d74334a5
elixirevo <elixirevocation@gmail.com>
2021-10-18 06:24:34 +0900

    update: mumbai testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -16,6 +16,10 @@ module.exports = {
     gorli: {
       url: 'https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
       accounts: [process.env.TestPK || ''],
+    },
+    mumbai: {
+      url: 'https://rpc-mumbai.maticvigil.com',
+      accounts: [process.env.TestPK || ''],
     }
   },
   abiExporter: {
