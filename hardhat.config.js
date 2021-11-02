39c2393c2a8724d126f24ff58305b4790a8fe90a
elixirevo <elixirevocation@gmail.com>
2021-11-02 23:32:12 +0900

    update: setting, readme

--- hardhat.config.js
+++ hardhat.config.js
@@ -12,6 +12,7 @@ module.exports = {
     polygon: {
       url: 'https://polygon-rpc.com',
       accounts: [process.env.PK || ''],
+      gas: 35,
     },
     gorli: {
       url: 'https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
