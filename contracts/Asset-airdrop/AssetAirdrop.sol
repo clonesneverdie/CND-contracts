23601389395f63658d2693f7a195307ca505ffd9
elixirevo <elixirevocation@gmail.com>
2021-11-07 13:26:15 +0900

    add: asset airdrop

---
+++ contracts/Asset-airdrop/AssetAirdrop.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+import "../CND-Asset/ICNDAsset.sol";
+
+contract AssetAirdrop {
+	ICNDAsset public Asset;
+	address public assetStoredAddress;
+
+	constructor(address _assetStoredAddress, address _assetContractAddress) {
+		devTeam = msg.sender;
+		contractAddress = address(this);
+		Asset = ICNDAsset(_assetContractAddress);
+		assetStoredAddress = _assetStoredAddress;
+	}
+
+	function partsAirdrop(uint256 tokenId, address[] memory user) public onlyDev {
+		for (uint256 i = 0; i < user.length; i++) {
+			address reciever = user[i];
+			Asset.safeTransferFrom(assetStoredAddress, reciever, tokenId, 1, "");
+		}
+	}
+}
