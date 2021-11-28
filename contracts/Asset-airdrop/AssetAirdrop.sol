88ad03485084c3df755d38a7985730755d0b4d3f
elixirevo <elixirevocation@gmail.com>
2021-11-29 02:27:45 +0900

    fix: asset airdrop

--- contracts/Asset-airdrop/AssetAirdrop.sol
+++ contracts/Asset-airdrop/AssetAirdrop.sol
@@ -6,7 +6,14 @@ import "../CND-Asset/ICNDAsset.sol";
 
 contract AssetAirdrop {
 	ICNDAsset public Asset;
+	address public contractAddress;
+	address public devTeam;
 	address public assetStoredAddress;
+
+	modifier onlyDev() {
+		require(msg.sender == devTeam);
+		_;
+	}
 
 	constructor(address _assetStoredAddress, address _assetContractAddress) {
 		devTeam = msg.sender;
